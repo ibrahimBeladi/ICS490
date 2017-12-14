00b242a153f5eb19a006611abc0f8663c0fda1fb
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-12-15 01:58:10 +0300

    arabization

--- project/hash/readTweet.py
+++ project/hash/readTweet.py
@@ -1,19 +1,27 @@
 #encoding: utf-8
-import csv, getpass, json, os, time, urllib
+import csv, getpass, json, os, time, urllib, errno
 from tweepy import Cursor
-#from twitter_client import get_twitter_client
 from twitter_client import get_twitter_client
-
-
+###########################################################################################
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+###########################################################################################
 if __name__ == '__main__':
 
+    mkdir_p("rawdata")
     fname = "ids.jsonl"
     client = get_twitter_client()
     with open(fname, 'r') as f:
         for line in f:
             id_1 = json.loads(line)
-            outName = "{}.json".format(id_1)
-            status = client.get_status(id_1)
+            outName = "{}".format(id_1)
+            status = client.get_status(id_1,tweet_mode='extended')
             with open("rawdata"+"/"+outName, 'w') as f:
-                f.write(json.dumps(status._json))
+                f.write(status.full_text.encode('utf-8'))
 
