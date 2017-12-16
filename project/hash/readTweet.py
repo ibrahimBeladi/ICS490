846771cac158d25109853c3ce2519b0071965308
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-12-16 07:02:50 +0300

    better error flow

--- project/hash/readTweet.py
+++ project/hash/readTweet.py
@@ -1,5 +1,5 @@
 #encoding: utf-8
-import csv, getpass, json, os, time, urllib, errno
+import csv, getpass, json, os, time, urllib, errno, os.path, sys
 from tweepy import Cursor
 from twitter_client import get_twitter_client
 ###########################################################################################
@@ -12,19 +12,43 @@ def mkdir_p(path):
         else:
             raise
 ###########################################################################################
+def write(path, msg):
+    with open(path, 'w') as f:
+        f.write(msg.encode('utf-8'))
+###########################################################################################
 if __name__ == '__main__':
 
-    mkdir_p("rawdata")
-    fname = "not_exported"
+    destination = "rawdata"
+    fname = "list"
+
+    mkdir_p(destination)
     client = get_twitter_client()
+
     with open(fname, 'r') as f:
         for line in f:
             id_1 = json.loads(line)
             outName = "{}".format(id_1)
-            try:
-                status = client.get_status(id_1,tweet_mode='extended')
-                with open("rawdata"+"/"+outName, 'w') as f:
-                    f.write(status.full_text.encode('utf-8'))
-            except:
-                print "Error at "+line
+            if not os.path.exists(destination+"/"+outName):
+                try:
+                    status = client.get_status(id_1,tweet_mode='extended')
+                    write(destination+"/"+outName, status.full_text)
+                except Exception as e: 
+                    if e[0][0]['code'] == 144:
+                        print outName + " is deleted"
+                        write(destination+"/"+outName, "")
+                    elif e[0][0]['code'] == 179:
+                        print outName + " is private"
+                        write(destination+"/"+outName, "")
+                    elif e[0][0]['code'] == 63:
+                        print outName + " user's is suspended"
+                        write(destination+"/"+outName, "")
+                    elif e[0][0]['code'] == 34:
+                        print outName + " that page does not exist."
+                        write(destination+"/"+outName, "")
+                    elif e[0][0]['code'] == 88:
+                        sys.exit("Rate limit exceeded, before reaching: " + outName)
+                    else:
+                        print e
+                        print "for " + outName
+                        sys.exit("New Error")
 
