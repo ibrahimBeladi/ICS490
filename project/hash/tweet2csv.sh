87cd4db58129d57434a47562f3e961b9fe4e12b5
Ibrahim Al-Beladi <ibra5him@hotmail.com>
2017-12-16 07:02:04 +0300

    (bash) now excludes empty, exported or retweeted tweets

--- project/hash/tweet2csv.sh
+++ project/hash/tweet2csv.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+
 directory=$1
 
 csv=`basename $directory`".csv"
-# Clears the csv file
-echo 'id,status'> $csv
+exportedList=exportedList
 
-for i in `dir $directory` ; do
-    # list the tweets in the csv
-    (echo -n $i, && cat $directory/$i | tr '\n' ' '&& echo) >> $csv
+echo 'id,status' > $csv
+
+for tweetID in `dir $directory` ; do
+    if [ -s $directory/$tweetID ] # if not empty
+    then
+        if !(grep -q "$tweetID" $exportedList) # if not exported
+        then
+            if !(cat $directory/$tweetID  | grep -q "RT @") # if not retweeted
+            then
+                (echo -n $tweetID, && cat $directory/$tweetID | tr '\n' ' '&& echo) >> $csv
+            fi
+        fi
+    fi
 done
